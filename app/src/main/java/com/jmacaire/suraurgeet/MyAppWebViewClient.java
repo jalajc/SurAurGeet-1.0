7d3b0767036d7e725845f3a3f63733f5fd1b3b35
JC <jalaj.chhalotre@gmail.com>
2015-09-15 12:26:48 +0530

    Hello World Version
    Hello World Version

---
+++ app/src/main/java/com/jmacaire/suraurgeet/MyAppWebViewClient.java
@@ -0,0 +1,21 @@
+package com.jmacaire.suraurgeet;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MyAppWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (Uri.parse(url).getHost().endsWith("jalaj.in")) {
+            return false;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;
+    }
+}
